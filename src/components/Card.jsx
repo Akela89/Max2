031a97a99a01d6f7d54623be3d9f175474d289f1
Ilya Soloviev <ilya0960@gmail.com>
2023-03-02 23:42:43 +0300

    remade semantics

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,21 @@ import classes from './Card.module.css'
 
 const Card = ( ) => {
     return (
-            <div className={classes.mainCard}>
-                <div className={classes.mainImg}></div>
-                    <div className={classes.wrapperInfo}>
-                        <p className={classes.titleCard}>React</p>
-                        <p className={classes.textCard}>by<a className={classes.cardLink} href="https://vk.com/i.solovyev89">Soloviev Ilya</a></p>
-                        <div className={classes.lineCard}></div>
-                        <div className={classes.artInfo}>Art Direction, Crafts, Fine Arts</div>
-                        <div className={classes.likesCard}>Any text</div>
-                    </div>
-            </div> 
+        <div className={classes.mainCard}>
+            <div className={classes.mainImg}></div>
+                <div className={classes.wrapperInfo}>
+                    <p className={classes.titleCard}>React</p>
+                    <p className={classes.textCard}>
+                        by
+                        <a className={classes.cardLink} href="https://vk.com/i.solovyev89">
+                        Soloviev Ilya
+                        </a>
+                    </p>
+                    <div className={classes.lineCard}></div>
+                    <div className={classes.artInfo}>Art Direction, Crafts, Fine Arts</div>
+                    <div className={classes.likesCard}>Any text</div>
+                </div>
+        </div> 
     )
 }
 export default Card;
