1e5f202802ebdc13eb42eee0eb13687e6e0790c7
Ilya Soloviev <ilya0960@gmail.com>
2023-03-09 21:14:57 +0300

    add email and pass

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,22 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './Forms.module.css';
 
 const Forms = ( ) => {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const[emailDirty, setEmailDirty] = useState(false)
+    const[passwordDirty, setPasswordlDirty] = useState(false)
+    const[emailError, setEmailError] = useState('Поле ввода не может быть пустым')
+    const[passwordError, setPasswordError] = useState('Поле ввода не может быть пустым')
+
+    const emailHandler = (e)=>{
+        setEmail(e.target.value)
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (!re.test(String(e.target.value).toLowerCase())){
+                setEmailError("Некорректный емейл")
+            } else {
+                setEmailError("")
+            }
+    }
+
+    const passwordHeadler = (e) =>{
+        setPassword(e.target.value)
+        if (e.target.value.length < 3 || e.target.value.length < 8){
+        setPasswordError("Пароль должен быть длиннее 3 символов и не меньше 8")
+            if (!e.target.value){
+                setPasswordError("Поле ввода не может быть пустым")
+            }
+        }   else {
+                setPasswordError("")
+            }
+    }
+
+
+    const blurHundler = (e) => {
+        switch(e.target.name){
+            case 'email': setEmailDirty(true)
+                break
+            case 'password': setPasswordlDirty(true)
+                break
+            }
+    }
+
     return (
         <form>
         <div className={classes.formsWrapper}>
             <div className={classes.formsBorder}>
-                    <p className={classes.formText}>First name:</p> 
-                    <input type="text" name="firstName" />
+                    <p className={classes.formText}>Email:</p> 
+                    {(emailDirty && emailError) && <div className={classes.errorBlock}>{emailError}</div>}
+                    <input 
+                    value={email}
+                    onChange={e => emailHandler(e)}
+                    onBlur={e => blurHundler(e)}
+                    type="text" 
+                    name="email"
+                    placeholder="Enter your email"
+                    />
+                    
                     <br /><br />
-                    <p className={classes.formText}>Last name</p> 
-                    <input type="text" name="lastName"/>
+
+                    <p className={classes.formText}>Password</p> 
+                    {(passwordDirty && passwordError) && <div className={classes.errorBlock}>{passwordError}</div>}
+                    <input 
+                    onChange={e=>passwordHeadler(e)}
+                    value={password}
+                    onBlur={e => blurHundler(e)} 
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"/>
+
                     <br /> <br />
+
+
+                    <p className={classes.formText}>Phone:</p> 
+                    <input 
+                    type="tel" 
+                    name="phone"
+                    placeholder="Phone Number"/>
+                    <br /><br />
 
 
                     <p className={classes.formText}>Birthday:</p> 
                     <input type="date" name="birthday" />
-
 
 
                     <p className={classes.formText}>Сhoose the right one:</p> 
@@ -24,8 +88,7 @@ const Forms = ( ) => {
                         <option value="html">HTML</option>
                         <option value="css">CSS</option>
                         <option value="js">JS</option>
-                        <option value="reactJs">React JS</option>
-                        
+                        <option value="reactJs">React JS</option>   
                     </select>
 
 
