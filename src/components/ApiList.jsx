edde932cf362aa436b7b10b429d267c742287bfc
Ilya Soloviev <ilya0960@gmail.com>
2023-04-12 19:32:56 +0300

    fix time

--- src/components/ApiList.jsx
+++ src/components/ApiList.jsx
@@ -3,12 +3,10 @@ import classes from './Forms.module.css';
 import ApiWeatherInfo from "./ApiWeatherInfo";
 import ApiForm from "./ApiForm";
 
-
 const apiKey = '986b337467ac73faba2c5e2464a7fa47';
 
-
 const ApiList = () => {
-
+    
     const [state, setState] = useState({
         temp: undefined,
         city: undefined,
@@ -26,17 +24,22 @@ const ApiList = () => {
             const apiUrl = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
             const data = await apiUrl.json();
 
-            // let sunset = data.sys.sunset;
-            // let date = new Date();
-            // date.setTime(sunset);
-            // let sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+            let sunset = data.sys.sunset;
+            let date = new Date();
+            date.setTime(sunset);
+            let sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+
+            let sunrise = data.sys.sunrise;
+            let date_s = new Date();
+            date.setTime(sunrise);
+            let sunrise_date = date_s.getHours() + ":" + date_s.getMinutes() + ":" + date_s.getSeconds()
             
             setState({
                 temp: data.main.temp,
                 city: data.name,
                 country: data.sys.country,
-                sunrise: data.sys.sunrise,
-                sunset: data.sys.sunset,
+                sunrise: sunrise_date,
+                sunset: sunset_date,
                 error: undefined,
             });
         } else{
